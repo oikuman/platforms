53ae39d9ace454c9c66aaa675f179a3494e60eaf
Oleksandr Krasnostup <oikuman@gmail.com>
2023-11-16 20:15:26 +0200

    features

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -22,35 +22,35 @@ const Features = () => {
             xs={12} style={{padding: 0}}
             // className="d-flex align-items-center justify-content-center"
           >
-            <ul className="list d-flex align-items-start justify-content-between flex-column justify-content-sm-start flex-sm-row justify-content-md-between features-box">
+            <ul className="list d-flex align-items-start justify-content-between flex-column justify-content-md-start flex-md-row justify-content-lg-between features-box">
 
-              <li className="list-item d-flex align-items-center justify-content-center flex-row flex-sm-column flex-md-row">
+              <li className="list-item d-flex align-items-center justify-content-center flex-row flex-md-column flex-lg-row">
                 <RoundIcon icon={<ClockwiseIcon />} radius={12} />
-                <span className="text-white text-start text-sm-center text-md-start features-margin small">
+                <span className="text-white text-start text-md-center text-lg-start features-margin small">
                   <span className="small light">Розробка займає</span><br/>
                   <span className="small light">до 2 місяців</span>
                 </span>
               </li>
 
-              <li className="list-item d-flex align-items-center justify-content-center flex-row flex-sm-column flex-md-row">
+              <li className="list-item d-flex align-items-center justify-content-center flex-row flex-md-column flex-lg-row">
               <RoundIcon icon={<UsersThreeIcon />} radius={12} />
-                <span className="text-white text-start text-sm-center text-md-start features-margin small">
+                <span className="text-white text-start text-md-center text-lg-start features-margin small">
                 <span className="small light">Кваліфікована</span><br/>
                   <span className="small light">команда спеціалістів</span>
                 </span>
               </li>
 
-              <li className="list-item d-flex align-items-center justify-content-center flex-row flex-sm-column flex-md-row">
+              <li className="list-item d-flex align-items-center justify-content-center flex-row flex-md-column flex-lg-row">
               <RoundIcon icon={<LogisticIcon />} radius={12} />
-                <span className="text-white text-start text-sm-center text-md-start features-margin small">
+                <span className="text-white text-start text-md-center text-lg-start features-margin small">
                 <span className="small light">Логічна</span>{" "}
                   <span className="small light">структура</span><br/><span className="small light">платформи</span>
                 </span>
               </li>
 
-              <li className="list-item d-flex align-items-center justify-content-center flex-row flex-sm-column flex-md-row">
+              <li className="list-item d-flex align-items-center justify-content-center flex-row flex-md-column flex-lg-row">
               <RoundIcon icon={<ShieldIcon />} radius={12} />
-                <span className="text-white text-start text-sm-center text-md-start features-margin small">
+                <span className="text-white text-start text-md-center text-lg-start features-margin small">
                 <span className="small light">Висока</span>{" "}
                   <span className="small light">безпека</span><br/><span className="small light">даних</span>
                 </span>
